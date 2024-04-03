508e0ccbc34acd6a4e957749861af73038886b4c
BomanStatic <boman.staticsites@gmail.com>
2024-04-03 18:14:10 +0200

    more boilerplate

--- src/router/chatRouter.js
+++ src/router/chatRouter.js
@@ -1,25 +1,26 @@
 import express from "express";
+import chatController from "../controller/chatController.js";
 const router = express.Router();
 
 // GET	/api/broadcast	hämta alla meddelanden som skickats till broadcast kanalen
-router.get("/api/broadcast");
+router.get("/api/broadcast", chatController.getBroadcastMessages);
 
 // POST	/api/broadcast	skapa ett nytt meddelande i broadcast kanalen
-router.post("/api/broadcast");
+router.post("/api/broadcast", chatController.createBroadcastMessage);
 
 // GET	/api/channel/	hämtar en lista över kanaler.
-router.get("/api/channel");
+router.get("/api/channel", chatController.getChannels);
 
 // GET	/api/channel/:id	hämtar alla meddelanden i specifik kanal
-router.get("/api/channel/:id");
+router.get("/api/channel/:id", chatController.getChannelMessages);
 
 // PUT	/api/channel/	skapar en ny kanal. Kanalens namn ska skickas med.
-router.put("/api/channel");
+router.put("/api/channel", chatController.createChannel);
 
 // POST	/api/channel/:id	skapa ett nytt meddelande i en specifik kanal som tidigare har skapats. Innehållet i ett meddelande bör vara minst anvsändare och innehåll.
-router.post("/api/channel/:id");
+router.post("/api/channel/:id", chatController.createChannelMessage);
 
 // DELETE	/api/channel/:id	tar bort en identiferad kanal som tidigare annonserats ut. (kräver auth) VG
-router.delete("/api/channel/:id");
+router.delete("/api/channel/:id", chatController.deleteChannel);
 
 export default router;
---
+++ src/service/chatService.js
@@ -0,0 +1,34 @@
+import { fetchCollection } from "../mongodb/mongoClient";
+
+const CHAT_COLLECTION_NAME = "";
+
+const fetchBroadcastMessages = () => {
+    return "not yet implemented";
+};
+const createNewBroadcastMessage = () => {
+    return "not yet implemented";
+};
+const fetchAllChannels = () => {
+    return "not yet implemented";
+};
+const fetchChannelMessages = (id) => {
+    return "not yet implemented";
+};
+const createNewChannel = () => {
+    return "not yet implemented";
+};
+const createNewChannelMessage = (id) => {
+    return "not yet implemented";
+};
+const removeChannel = (id) => {
+    return "not yet implemented";
+};
+export default {
+    fetchBroadcastMessages,
+    createNewBroadcastMessage,
+    fetchAllChannels,
+    fetchChannelMessages,
+    createNewChannel,
+    createNewChannelMessage,
+    removeChannel,
+};
