2ffeb5d06271386df81cd923567a54d6e422dba9
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-04-05 09:55:26 +0200

    Created the auth for the app

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import chatRouter from "./src/router/chatRouter.js";
+import authRouter from "./src/router/authRouter.js";
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,7 @@ const PORT = 3000;
 app.use(express.json());
 
 app.use(chatRouter);
+app.use(authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is live on http://localhost:${PORT}`);
---
+++ src/controller/authController.js
@@ -0,0 +1,33 @@
+import authService from "../service/authService.js";
+
+// Takes in username and password from the body and then checks if it is undefined. Then sends them to the service
+const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === undefined && password === undefined) {
+        return res.status(400).send({ err: "invalid parameters" });
+    }
+
+    try {
+        const token = await authService.exists({ username, password });
+        res.status(200).send({ token });
+    } catch (e) {
+        return res.status(401).send({ msg: "Failed to login" });
+    }
+};
+
+// Takes in username and password from the body and then checks if it is undefined. Then sends them to the service
+const signUp = async (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === undefined && password === undefined) {
+        return res.status(400).send({ err: "invalid parameters" });
+    }
+
+    await authService.create({ username, password });
+
+    return res.status(201).send({ msg: "Account was created" });
+};
+
+// exports login, signUp
+export default { login, signUp };
